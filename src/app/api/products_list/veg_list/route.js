e5db3b1e048ca9a44bd1eb259bd7c5b8b9e03196
BhavaniVelraj <bhavanivnm25@gmail.com>
2025-07-16 11:01:51 +0530

    final

--- src/app/api/products_list/veg_list/route.js
+++ src/app/api/products_list/veg_list/route.js
@@ -2,6 +2,7 @@
 import { querys } from '@/src/app/lib/DbConnection';
 import { verifyToken } from '@/src/app/lib/Token';
 import { NextResponse } from 'next/server';
+export const dynamic = "force-dynamic";
 
 export async function GET(req) {
     try {
